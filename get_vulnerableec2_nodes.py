929b790ec1a0664bf6499a7b6b0976d96cb577af
Niranjan Alur <69748812+anniranjan@users.noreply.github.com>
2024-04-27 15:21:42 +0530

    Added comments and removed unused statements

--- get_vulnerableec2_nodes.py
+++ get_vulnerableec2_nodes.py
@@ -18,43 +18,41 @@ def get_ec2_instances():
             # Append the modified instance to the instances list
             instances.append(instance)
     kubernetes_nodes = []
+    #Iterate through the nodes and filter kubernetes nodes
     for instance in instances:
         if 'aws:eks:cluster-name' in [tag['Key'] for tag in instance.get('Tags', [])]:
             kubernetes_nodes.append(instance)
-   # print(kubernetes_nodes)
     return kubernetes_nodes
-    #return instances
 
 def get_k8s_resources():
-    config.load_kube_config()  # Assuming kubeconfig is set up
+    #Load the kube config
+    config.load_kube_config()
     v1 = client.CoreV1Api()
+    #get the pod lists
     pods = v1.list_pod_for_all_namespaces(watch=False)
-    services = v1.list_service_for_all_namespaces(watch=False)
-    return pods.items, services.items
+    #get the services list
+    services = v1.list_service_for_all_namespaces(watch=False, pretty="True")
+    return f"{pods.items}", f"{services.items}"
 
 def evaluate_with_rego(instance_data, rego_script_path):
-    # Assuming OPA binary is available and rego script is prepared
+    # Run the opa evaluation process
     with open('data.json', 'w') as file:
         json.dump(instance_data, file, indent=4, sort_keys=True, default=str)
 
-    result = subprocess.run(['opa', 'eval', '--data', rego_script_path, '--input', 'data.json', 'true', '--format', 'json'],
+    result = subprocess.run(['opa', 'eval', '--data', rego_script_path, '--input', 'data.json', 'data.ec2evaluation', '--format', 'json'],
                             capture_output=True, text=True)
-    print(result)
     output = result.stdout
     parsed_output = json.loads(output)
-    #return result.stdout.strip() == 'true'
     return parsed_output['result'][0]['expressions'][0]['value']
 
 def main():
+    #Get the EC2 instances
     ec2_instances = get_ec2_instances()
-    #print(ec2_instances)
+    #Get the pods and services running in EKS cluster
     pods, services = get_k8s_resources()
-    #print(pods)
-    #print(services)
     vulnerable_instances = []
+    #Run the policy
     for instance in ec2_instances:
-        security_group_ids = [sg['GroupId'] for sg in instance['SecurityGroups']]
-        print(security_group_ids)
 
         rego_script_path = 'policy.rego'
 
@@ -67,7 +65,7 @@ def main():
             vulnerable_instances.append(instance['InstanceId'])
             print("Vulnerable EC2: ", instance['InstanceId'])
         else:
-            print("Else block")
+            print("EC2 is not vulnerable")
     print("Vulnerable EC2 Instances:", vulnerable_instances)
 
 if __name__ == '__main__':
